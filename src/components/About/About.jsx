a2e7e8eb74685f5bd15d5dd588185c1f33d84657
Leah <leahb_96@hotmail.co.uk>
2023-04-06 22:28:50 +0100

    added icons and subheadings

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,21 +1,28 @@
 import "./About.css";
 import AboutCard from "../AboutCard/AboutCard";
+import { FaUserGraduate, FaLaptopCode, FaUsers } from "react-icons/fa";
 
 const aboutCards = [
   {
     heading: "Northcoders Graduate",
-    info: "info here",
+    subheading: "480 contact hours during a 13 week bootcamp",
+    info: "Northcoders is an industry-leading software development training provider.",
     skills: ["list skills here"],
+    icon: <FaLaptopCode />
   },
   {
-    heading: "Coding Projects",
+    heading: "Customer Experience Professional",
+    subheading: "10 years experience in various Customer Happiness, Visitor Experience and Managment roles",
     info: "info here",
     skills: ["list skills here"],
+    icon: <FaUsers />
   },
   {
     heading: "Bachelor of Arts",
+    subheading: "Graduated with a First Class Degree in Fine Art Mixed Media",
     info: "info here",
     skills: ["list skills here"],
+    icon: <FaUserGraduate />
   },
 ];
 
--- src/components/AboutCard/AboutCard.jsx
+++ src/components/AboutCard/AboutCard.jsx
@@ -1,14 +1,15 @@
 import "./AboutCard.css";
-import { FaUserGraduate, FaLaptopCode } from "react-icons/fa";
+
 
 export default function AboutCard({ card }) {
-  const { heading, info, skills } = card;
+  const { heading, subheading, info, skills, icon } = card;
 
   return (
     <section>
       <li className="about-card" style={{ listStyle: "none" }}>
         <div>
-          <h3>{heading}</h3>
+          <h3>{heading} {icon}</h3>
+          <small>{subheading}</small>
           <p>{info}</p>
           <ul className="skills" style={{ listStyle: "none" }}>
             {skills.map((skill) => (
@@ -16,44 +17,7 @@ export default function AboutCard({ card }) {
             ))}
           </ul>
         </div>
-        {/* <div className="project-links">
-      {code && (
-        <a href={code} target="_blank" rel="noreferrer">
-          <FaGithub />
-        </a>
-      )}
-      {website && (
-        <a href={website} target="_blank" rel="noreferrer">
-          <FaExternalLinkAlt />
-        </a>
-      )}
-    </div> */}
       </li>
     </section>
   );
-
-  //   <section id="about-cards">
-  //     <h2>About Me</h2>
-  //     <div className="about-card-container">
-  //       <ul className="about-card">
-  //         <li>
-  //           <p>bio</p>
-  //         </li>
-  //         <li className="about-nc">
-  //           <FaLaptopCode className="code-icon" />
-  //           <h5>Northcoders Graduate</h5>
-  //           <small>480 Contact hours</small>
-  //         </li>
-  //         <li className="about-projects">
-  //           <h5>Projects</h5>
-  //           <small>Solo and group projects completed</small>
-  //         </li>
-  //         <li className="about-grad">
-  //           <FaUserGraduate className="grad-icon" />
-  //           <h5>Bachelor of Arts</h5>
-  //           <small>First Class Honours</small>
-  //         </li>
-  //       </ul>
-  //     </div>
-  //   </section>;
 }
