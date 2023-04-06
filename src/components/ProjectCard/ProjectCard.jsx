3ea71229b58dc556e84b4151f773b39e13acb7ec
Leah <leahb_96@hotmail.co.uk>
2023-04-06 19:40:30 +0100

    created project cards

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,5 +1,35 @@
-import './ProjectCard.css'
+import "./ProjectCard.css";
+import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
 
-export default function ProjectCard () {
-    
+export default function ProjectCard({ project }) {
+  const { title, info, tech, code, website } = project;
+  return (
+    <section>
+      <li className="project-card">
+        <div>
+          <h3>{title}</h3>
+          <header>
+            <ul className="tech">
+              {tech.map((item) => (
+                <li key={item}>{item}</li>
+              ))}
+            </ul>
+          </header>
+          <p>{info}</p>
+        </div>
+        <div className="project-links">
+          {code && (
+            <a href={code} target="_blank" rel="noreferrer">
+              <FaGithub />
+            </a>
+          )}
+          {website && (
+            <a href={website} target="_blank" rel="noreferrer">
+              <FaExternalLinkAlt />
+            </a>
+          )}
+        </div>
+      </li>
+    </section>
+  );
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,51 @@
 import './Projects.css'
+import ProjectCard from '../ProjectCard/ProjectCard'
 
 export default function Projects () {
-    
+    const projects = [
+        {
+            title: "NC News",
+            info: "NC News is an interactive web application built using React. It serves as a news aggregation and comments board, allowing users to log in, browse the various articles by topic/popularity and get involved in the discussion. The app uses the NC News API to fetch the stories and allow the user to interact with them via comments and votes.",
+            tech: ["Javascript", "React"],
+            code: "https://github.com/leahbanks/FE_nc_news",
+            website: "https://nc-news-lb.netlify.app/",
+        },
+        {
+            title: "NC News API",
+            info: "NC News API is a RESTful API built using Javascript, Node.js, PostgreSQL and Express.js. It has been designed to provide the functionality behind and interactive web application, which will serve as a Reddit-style news aggregation and comments board.",
+            tech: ["Javascript", "PostgreSQL", "Express.js"],
+            code: "https://github.com/leahbanks/BE_nc_news",
+            website: "https://nc-news-81lq.onrender.com/api",
+        },
+        {
+            title: "Through the Fog",
+            info: "",
+            tech: [],
+            code: "https://github.com/leahbanks/FE-through-the-fog"
+        },
+        {
+            title: "Through the Fog API",
+            info: "",
+            tech: [],
+            code: "https://github.com/leahbanks/BE-through-the-fog"
+        },
+        {
+            title: "Portfolio",
+            info: "",
+            tech: [],
+            code: "https://github.com/leahbanks/portfolio",
+            website: "",
+        }
+    ];
         return (
             <section className="projects">
+                <h2>Projects</h2>
+    
+                <div className="project-container">
+                    {projects.map((project) => (
+                        <ProjectCard key={project.title} project={project} />
+                    ))}
+                </div>
             </section>
         );
     };
