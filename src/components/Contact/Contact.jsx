690f3608f2a8d8add576057617c2afee1d71e310
Leah <leahb_96@hotmail.co.uk>
2023-04-07 11:50:30 +0100

    created basic contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,46 @@
 import "./Contact.css";
 import { FaLinkedinIn } from "react-icons/fa";
-import {HiOutlineMail} from "react-icons/hi"
+import { HiOutlineMail } from "react-icons/hi";
 
 export default function Contact() {
+  const sendMessage = (e) => {
+    e.preventDefault();
+    e.target.reset();
+  };
   return (
-    <section className="about-links">
-      <h2>Get In Touch</h2>
-      <FaLinkedinIn />
-      <HiOutlineMail />
+    <section>
+      <div className="about-links">
+        <h2>Get In Touch</h2>
+        <FaLinkedinIn />
+        <HiOutlineMail />
+      </div>
+      <div id="contact" className="container">
+        <h5>Get In Touch</h5>
+        <h2>Contact Me</h2>
+
+        <form
+          onSubmit={(e) => {
+            sendMessage(e);
+          }}
+        >
+          <input type="text" name="name" placeholder="Your Name" required />
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email Address"
+            required
+          />
+          <textarea
+            name="message"
+            rows="8"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
     </section>
   );
 }
