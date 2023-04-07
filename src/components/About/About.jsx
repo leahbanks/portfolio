53b8f43415f8d7117ca854706555467ee06c0e22
Leah <leahb_96@hotmail.co.uk>
2023-04-07 13:18:24 +0100

    small css tweaks

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,16 @@
 import "./About.css";
 import AboutCard from "../AboutCard/AboutCard";
 import { aboutCards } from "../../data/about-data";
-import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 
 export default function About() {
   return (
     <section className="about">
       <h2>About Me</h2>
-      <FaLinkedinIn />
-      <FaGithub />
+        <button className="resume-btn">
+          <a href="CV_LeahBanks.pdf" target="_blank">
+            CV
+          </a>
+        </button>
       <p>Bio here...</p>
       <div className="about-container">
         {aboutCards.map((card) => (
